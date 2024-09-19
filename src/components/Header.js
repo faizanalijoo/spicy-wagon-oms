b4ada25934dcdea108fde3e75d29acb8b21f74c5
Rama Ganesh <Ramg@ucchvas.com>
2024-09-19 13:29:25 +0530

    Vikrayin-06: Adding Vendor Name and option to select Vendor when more than 1 are present

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,15 +6,17 @@ import {
   Menu,
   MenuItem,
   Box,
+  Typography,
+  Button,
 } from "@mui/material";
-import { AccountCircle } from "@mui/icons-material";
+import { AccountCircle, ExpandMore } from "@mui/icons-material";
 import { useAuth } from "../contexts/AuthContext";
 import styled from "styled-components";
 import { useTheme } from "@mui/material/styles";
 import logo from "../images/logo.png";
 
 const StyledAppBar = styled(AppBar)`
-  background-color: #1e1e1e;
+  background-image: linear-gradient(to bottom right, #791717, #2a0101);
   z-index: ${(props) =>
     props.theme.zIndex.drawer + 1}; // Ensure AppBar is above Sidebar
 `;
@@ -25,17 +27,49 @@ const LogoImage = styled.img`
   width: auto;
 `;
 
+const Logo = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  marginRight: "auto",
+});
+
+const OutletSelector = styled(Button)({
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+  color: "white",
+  textTransform: "none",
+  marginRight: "16px",
+  "&:hover": {
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+  },
+});
+
 const Header = () => {
-  const { logout } = useAuth();
+  const { logout, vendors } = useAuth();
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorElOutlet, setAnchorElOutlet] = React.useState(null);
+  const [selectedOutlet, setSelectedOutlet] = useState(vendors[0].outlet.name);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOutletClick = (event) => {
+    setAnchorElOutlet(event.currentTarget);
+  };
+
+  const handleOutletClose = () => {
+    setAnchorElOutlet(null);
+  };
+
+  const handleOutletChange = (outlet) => {
+    setSelectedOutlet(outlet);
+    handleClose();
+  };
+
   const theme = useTheme();
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
   };
 
   return (
@@ -44,6 +78,33 @@ const Header = () => {
         <Box sx={{ flexGrow: 1 }}>
           <LogoImage src={logo} alt="SpicyWagon Logo" />
         </Box>
+        <Logo>
+          <Typography variant="h7" component="div">
+            {selectedOutlet}
+          </Typography>
+        </Logo>
+        {vendors.length > 1 && (
+          <>
+            <OutletSelector
+              endIcon={<ExpandMore />}
+              onClick={handleOutletClick}
+              disabled={vendors.length < 2}
+            >
+              Change Outlet
+            </OutletSelector>
+            <Menu
+              anchorEl={anchorElOutlet}
+              open={Boolean(anchorElOutlet)}
+              onClose={handleOutletClose}
+            >
+              {vendors.map((v) => (
+                <MenuItem onClick={() => handleOutletChange(v.outlet.name)}>
+                  {v.outlet.companyName}
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
+        )}
         <div>
           <IconButton
             size="large"
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useState, useEffect, useCallback } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
 import api from "../services/api";
 import { apiEndpoints } from "../services/apiEndpoints";
 
@@ -8,18 +14,33 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [userRole, setUserRole] = useState(null);
   const [vendorId, setVendorId] = useState(null);
+  const [vendors, setVendors] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchUserRole = useCallback(async (token) => {
-    try {
-      const response = await api.get(apiEndpoints.getRoles);
-      // setUserRole(response.data.role);
-      setVendorId(response.data[0].vendors[0]?.outlet?.outlet_id);
-      setLoading(false);
-    } catch (error) {
-      logout();
-    }
+  const logout = useCallback(() => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    setUserRole(null);
+    setVendors([]);
+    setVendorId(null);
   }, []);
+
+  const fetchUserRole = useCallback(
+    async (token) => {
+      try {
+        const response = await api.get(apiEndpoints.getRoles);
+        // Uncomment the next line if you want to set the user role
+        setVendors(response.data[0]?.vendors);
+        setVendorId(response.data[0]?.vendors[0]?.outlet?.outlet_id || null);
+      } catch (error) {
+        console.error("Error fetching user role:", error);
+        logout();
+      } finally {
+        setLoading(false);
+      }
+    },
+    [logout]
+  );
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -31,27 +52,35 @@ export const AuthProvider = ({ children }) => {
       setIsAuthenticated(false);
     }
   }, [fetchUserRole]);
- 
-  const login = async (token) => {
-    localStorage.setItem("token", token);
-    setIsAuthenticated(true);
-    await fetchUserRole(token);
-  };
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    setIsAuthenticated(false);
-    setUserRole(null);
-    setVendorId(null);
+  const login = useCallback(
+    async (token) => {
+      localStorage.setItem("token", token);
+      setIsAuthenticated(true);
+      await fetchUserRole(token);
+    },
+    [fetchUserRole]
+  );
+
+  const contextValue = {
+    isAuthenticated,
+    login,
+    logout,
+    userRole,
+    vendorId,
+    loading,
+    vendors
   };
 
   return (
-    <AuthContext.Provider
-      value={{ isAuthenticated, login, logout, userRole, vendorId, loading }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
