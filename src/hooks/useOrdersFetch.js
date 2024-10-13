d8e2a777194d5cfa5b68c47f9552ae4bf2d0eae9
Rama Ganesh <Ramg@ucchvas.com>
2024-10-13 21:42:22 +0530

    testing build

--- src/hooks/useOrdersFetch.js
+++ src/hooks/useOrdersFetch.js
@@ -1,6 +1,6 @@
 // useOrdersFetch.js
 import React from "react";
-import { useSpeechSynthesis } from "react-speech-kit";
+// import { useSpeechSynthesis } from "react-speech-kit";
 import { apiEndpoints } from "../services/apiEndpoints";
 import * as moment from "moment";
 import api from "../services/api";
@@ -10,7 +10,7 @@ export const useOrdersFetch = (outletId, latestOrderId = 0, count) => {
   const { refreshTime, isNotificationOn } = useVendor();
 
   const [newOrdersCount, setNewOrdersCount] = React.useState(0);
-  const { speak } = useSpeechSynthesis();
+  // const { speak } = useSpeechSynthesis();
 
   const fetchOrdersSinceLatest = React.useCallback(
     async (sinceId) => {
@@ -24,15 +24,16 @@ export const useOrdersFetch = (outletId, latestOrderId = 0, count) => {
         const data = response.data;
         setNewOrdersCount(data.count);
           if (data.count > 0 && isNotificationOn) {
-            speak({
-              text: `${data.count} new order${data.count > 1 ? "s" : ""} received`,
-            });
+            // speak({
+            //   text: `${data.count} new order${data.count > 1 ? "s" : ""} received`,
+            // });
+            console.log(`${data.count} new order${data.count > 1 ? "s" : ""} received`)
         }
       } catch (error) {
         console.error("Error fetching orders:", error);
       }
     },
-    [outletId, newOrdersCount, speak]
+    [outletId, newOrdersCount]
   );
 
   React.useEffect(() => {
